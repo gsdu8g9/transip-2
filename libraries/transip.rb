f8461a3a33f6ff5621893981c0f98f5f32f27a02
Jean Mertz <jean@mertz.fm>
2014-10-04 22:12:19 +0200

    Add both default and os-specific gem paths to LOAD_PATH

--- libraries/transip.rb
+++ libraries/transip.rb
@@ -1,27 +1,25 @@
 p  = `ohai platform | awk "NR==2"`.strip.delete('"')
 pv = `ohai platform_version | awk "NR==2"`.strip.delete('"')
 
-def expanded_gems(type = 'default')
-  cbpaths = [Chef::Config[:cookbook_path]].flatten
+def expanded_gems
+  p       = `ohai platform | awk "NR==2"`.strip.delete('"')
+  pv      = `ohai platform_version | awk "NR==2"`.strip.delete('"')
+  cbpath  = Chef::Config[:cookbook_path]
+  paths   = ['default', "#{p}-#{pv}", p].map! do |type|
+    File.join(cbpath, 'transip', 'files', type, 'vendor', 'gems', '**', 'lib')
+  end
 
-  Dir[*cbpaths.map! do |path|
-    File.join(path, 'transip/files/default/vendor/gems/**/lib')
-    File.join(path, "transip/files/#{type}/vendor/gems/**/lib")
-  end]
+  @expanded_gems ||= Dir[*paths]
 end
 
-if expanded_gems("#{p}-#{pv}").any? { |s| s.include?('gems/transip') }
-  $LOAD_PATH.unshift(*expanded_gems("#{p}-#{pv}"))
-  require 'transip'
-elsif expanded_gems(p).any? { |s| s.include?('gems/transip') }
-  $LOAD_PATH.unshift(*expanded_gems(p))
+if expanded_gems.any? { |s| s.include?('gems/transip') }
+  $LOAD_PATH.unshift(*expanded_gems)
   require 'transip'
 else
   File.directory?('/tmp/kitchen') || Chef::Application.fatal!(
-    'The `transip` cookbook does not support your current platform: '       \
-    "#{p}-#{pv}. Searched for the `transip` gem in these directories:\n "   \
-    "#{[expanded_gems("#{p}-#{pv}"), expanded_gems(p)].flatten.join("\n")}" \
-    "\nYou can add a request at "                                           \
+    'The `transip` cookbook does not support your current platform: '     \
+    "#{p}-#{pv}. Searched for the `transip` gem in these directories:\n " \
+    "#{expanded_gems.join("\n")}\n You can add a request at "             \
     'http://github.com/kabisa-cookbooks/transip/issues.', 1)
 end
 

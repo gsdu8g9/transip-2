b5c0813e149a461b63c3d96d694792cdf31dc9d9
Jean Mertz <jean@mertz.fm>
2014-10-04 22:11:36 +0200

    Remove wildcard domains because of TransIP API bug
    Due to an unknown bug in either the Ruby client, or the TransIP API, DNS entries
    with wildcards (*) in them, will fail the request:
    
      Transip::ApiError: Invalid API signature, signature does not match the
      request. (timestamp: 0.74032400 1412434601)
    
    These domains are therefor filtered out (and thus no longer work). This can be
    problematic, but until a workaround is possible, this is the only way to work
    with DNS entries.

--- libraries/dns_entry.rb
+++ libraries/dns_entry.rb
@@ -23,10 +23,37 @@ module Kabisa
       end
 
       def all
-        client.request(:get_info, domain_name: domain).dns_entries
+        filter_wildcards(domain_details.dns_entries)
       end
 
       private
+
+      def domain_details
+        client.request(:get_info, domain_name: domain)
+      end
+
+      #
+      # Due to an unknown bug in either the Ruby client, or the TransIP API, DNS
+      # entries with wildcards (*) in them, will fail the request:
+      #
+      #   Transip::ApiError: Invalid API signature, signature does not match the
+      #   request. (timestamp: 0.74032400 1412434601)
+      #
+      # These domains are therefor filtered out (and thus no longer work). This
+      # can be problematic, but until a workaround is possible, this is the only
+      # way to work with DNS entries.
+      #
+      def filter_wildcards(entries)
+        wildcards = entries.select { |entry| entry.name == '*' }
+
+        if wildcards.any?
+          Chef::Log.warn 'DNS entries with wildcards have been removed from '  \
+                         'your API query. The following entries where removed:'\
+                         "\n#{wildcards.join("\n")}"
+        end
+
+        entries - wildcards
+      end
 
       def save_entries(entries)
         client.request(:set_dns_entries, domain_name: domain,
